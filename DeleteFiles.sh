97144f871d77664e84fcd1eb6f50d665f321c631
Jan H. Krüger <janhkrueger@gmail.com>
2016-06-20 00:26:10 +0200

    Delte files of specific filetypes in a given directory on OSX

---
+++ DeleteFiles.sh
@@ -0,0 +1,46 @@
+# Shitty old script to clean directories on OSX
+
+find . -name "*.html" -delete
+find . -name "*.txt" -delete
+find . -name "*.png" -delete
+find . -name "*.jpg" -delete
+find . -name "*.css" -delete
+find . -name "*.gif" -delete
+find . -name "*.zip" -delete
+find . -name "*.doc" -delete
+find . -name "*.docx" -delete
+find . -name "*.xls" -delete
+find . -name "*.htm" -delete
+find . -name "*.ZIP" -delete
+find . -name "*.ppt" -delete
+find . -name "*.JPG" -delete
+find . -name "*.js" -delete
+find . -name "*.jpeg" -delete
+find . -name "*.mht" -delete
+find . -name "cv.pdf" -delete
+find . -name "*.HTM" -delete
+find . -name "*.ps" -delete
+find . -name "*.dvi" -delete
+find . -name "*.pps" -delete
+find . -name "*.tif" -delete
+find . -name "*.TIF" -delete
+find . -name "*.TIFF" -delete
+find . -name "*.tiff" -delete
+find . -name "*.gz" -delete
+find . -name "*.avi" -delete
+find . -name "*.exe" -delete
+find . -name "*.tex" -delete
+find . -name "*.db" -delete
+find . -name "*.GIF" -delete
+find . -name "*.bmp" -delete
+find . -name "*.xml" -delete
+find . -name "*.thmx" -delete
+find . -name "*.asc" -delete
+find . -name "*.bind" -delete
+find . -name "*.bat" -delete
+find . -name "*.lyx" -delete
+find . -name "*.LCK" -delete
+find . -name "*.mno" -delete
+find . -name ".DS_Store" -delete
+find . -name "Cv.pdf" -delete
+find /Volumes/VOLUMENAME/ -type d -empty -delete
